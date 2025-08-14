cbc4c0e8348b41081cf2831944b3eff72cef4362
Luiz Ribeiro <luizmarzolaribeiro@gmail.com>
2025-08-14 17:39:33 -0300

    atualização

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-require('dotenv').config();
-
 app.use(cors());
 app.use(express.json());
 
--- services/tarefaServices.js
+++ services/tarefaServices.js
@@ -1,27 +1,25 @@
 const db = require('../config/db');
-
+/*Função assíncrona para requisitar todas as tarefas do banco de dados*/
 async function listarQuery() {
-    const [resultado] = await db.query
-    ('SELECT * FROM tarefas');
-    return resultado;
+        const [resultado] = await db.query
+        ('SELECT * FROM tarefas');
+        return resultado; }
+
+async function criarQuery({ titulo, descricao, status, prioridade, data_entrega}) {
+        const query = 'INSERT INTO tarefas (titulo, descricao, status, prioridade, data_entrega) ' +
+        'VALUES (?, ?, ?, ?, ?)';
+        await db.query(query, [titulo, descricao, status, prioridade, data_entrega]);
 }
 
-async function criarQuery({ titulo, descricao, status, prioridade, dataEntrega}) {
-    const query = 'INSERT INTO tarefas (titulo, descricao, status, prioridade, dataEntrega)' + 
-    'VALUES (?, ? ,? ,?, ?)';
-    await db.query(query, [titulo, descricao, status, prioridade, dataEntrega]);    
+async function atualizarQuery(id, { titulo, descricao, status, prioridade, data_entrega }) {
+        const query = 'UPDATE tarefas SET titulo = ?, descricao = ?, status = ?, prioridade = ?, data_entrega = ? ' +
+        'WHERE id = ?';
+        const [resultado] = await db.query(query, [titulo, descricao, status, prioridade, data_entrega, id]);
+        return resultado.affectedRows;
 }
-
-async function atualizarQuery(id, { titulo, descricao, status, prioridade, dataEntrega}) {
-    const query = 'UPDATE tarefas SET titulo = ?, descricao = ?, status = ?, prioridade = ?, dataEntrega = ? ' +
-    'WHERE id = ?';
-    const [resultado] = await db.query(query, [titulo, descricao, status, prioridade, dataEntrega, id]);
-}
-
 async function deletarQuery(id) {
-    const query = 'DELETE FROM tarefas WHERE id = ?';
-    const [resultado] = await db.query(query, [id]);
-    return resultado.affectedRows;
+        const query = 'DELETE FROM tarefas WHERE id = ?';
+        const [resultado] = await db.query(query, [id]);
+        return resultado.affectedRows;
 }
-
 module.exports = { listarQuery, criarQuery, atualizarQuery, deletarQuery };
